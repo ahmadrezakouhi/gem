df7e2a5af0253b5624d85d80542c04c19b8039a4
ahmad reza kouhi <ahmadrezakouhi@gmail.com>
2022-08-15 10:44:37 +0430

    add all fields to vehicles migration

--- database/migrations/2022_08_13_072328_create_vehicles_table.php
+++ database/migrations/2022_08_13_072328_create_vehicles_table.php
@@ -15,6 +15,48 @@ return new class extends Migration
     {
         Schema::create('vehicles', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('panel_code');
+            $table->foreign('panel_code')->references('panel_code')->on('companies');
+            $table->string('number_plate',10)->nullable();
+            $table->string('number_plate_zone',10)->nullable();
+            $table->tinyInteger('number_plate_type')->nullable();
+            $table->bigInteger('numbering_location_code')->nullable();
+            $table->string('numbering_location')->nullable();
+            $table->string('VIN',50)->nullable();
+            $table->tinyInteger('allowed_driver_licence')->nullable();
+            $table->string('vehicle_type',50)->nullable();
+            $table->string('owner_mobile',20)->nullable();
+            $table->integer('production')->nullable();
+            $table->string('loading_type',50)->nullable();
+            $table->tinyInteger('type')->nullable();
+            $table->tinyInteger('status')->default(0);
+            $table->string('inspection_number',50)->nullable();
+            $table->date('inspection_expire')->nullable();
+            $table->string('insurance_number',50)->nullable();
+            $table->date('insurance_expire')->nullable();
+            $table->string('color')->nullable();
+            $table->bigInteger('brand_code')->nullable();
+            $table->string('brand_title',50)->nullable();
+            $table->bigInteger('type_code')->nullable();
+            $table->string('type_title')->nullable();
+            $table->integer('number_of_allowed_draft')->default(0)->unsigned();
+            $table->integer('day_of_allowed_draft')->default(0)->unsigned();
+            $table->integer('number_of_allowed_bill_of_lading')->default(0)->unsigned();
+            $table->integer('day_of_allowed_bill_of_lading')->default(0)->unsigned();
+            $table->integer('number_of_unconfirmed_draft')->default(0)->unsigned();
+            $table->integer('number_of_unconfirmed_bill_of_lading')->default(0)->unsigned();
+            $table->integer('capacity')->nullable();
+            $table->integer('weight')->nullable();
+            $table->integer('loading_length')->nullable();
+            $table->integer('loading_width')->nullable();
+            $table->tinyInteger('has_roll')->default(0)->unsigned();
+            $table->string('chassis_number',50)->nullable();
+            $table->string('document_number',50)->nullable();
+            $table->tinyInteger('violation')->default(0)->unsigned();
+            $table->text('description')->nullable();
+            $table->string('engine_number',50)->nullable();
+            $table->date('document_date')->nullable();
+            $table->tinyInteger('owner_status')->default(0)->unsigned();
             $table->timestamps();
         });
     }
