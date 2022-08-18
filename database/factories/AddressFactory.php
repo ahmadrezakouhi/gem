56698e08ee1823873f77d872a2cd2afb5b43e88e
ahmad reza kouhi <ahmadrezakouhi@gmail.com>
2022-08-18 10:52:46 +0430

    create AddressFactory

---
+++ database/factories/AddressFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\SenderReciver;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use \Ybazli\Faker\Facades\Faker;
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Address>
+ */
+class AddressFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'sender_reciver_id'=>SenderReciver::find(rand(1,19))->id,
+            'province'=>Faker::state(),
+            'city'=>Faker::city(),
+            'postal_code'=>fake()->numerify('#####-#####'),
+            'address'=>Faker::address(),
+            'status'=>fake()->numberBetween(0,2)
+        ];
+    }
+}
