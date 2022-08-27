b170d6743ef04239d6ea54bf8708041c6bc9478f
ahmad reza kouhi <ahmadrezakouhi@gmail.com>
2022-08-27 10:12:25 +0430

    create all factory in DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,12 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Address;
+use App\Models\Company;
+use App\Models\Driver;
+use App\Models\SenderReciver;
+use App\Models\Vehicle;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,7 +20,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(10)->create();
+        Company::factory(20)->create();
+        Driver::factory(20)->create();
+        SenderReciver::factory(20)->create();
+        Address::factory(20)->create();
+        Vehicle::factory(20)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
