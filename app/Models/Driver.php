bd6e4aa4711520ed4d3043d4e6b43d81d4ab3fbf
ahmad reza kouhi <ahmadrezakouhi@gmail.com>
2022-08-27 10:02:13 +0430

    convert all iso datetime in Driver model

--- app/Models/Driver.php
+++ app/Models/Driver.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Models;
-
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -9,4 +9,42 @@ class Driver extends Model
 {
     use HasFactory;
     protected $guarded = ['id'];
+
+    protected function driverLicenceExpire(): Attribute
+    {
+        return Attribute::make(
+            set: fn ($value) => convertJsonDate($value),
+        );
+    }
+
+    protected function healthCardExpire(): Attribute
+    {
+        return Attribute::make(
+            set: fn ($value) => convertJsonDate($value),
+        );
+    }
+
+
+    protected function smartNumberExpire(): Attribute
+    {
+        return Attribute::make(
+            set: fn ($value) => convertJsonDate($value),
+        );
+    }
+
+
+    protected function birthDate(): Attribute
+    {
+        return Attribute::make(
+            set: fn ($value) => convertJsonDate($value),
+        );
+    }
+
+    protected function startActivity(): Attribute
+    {
+        return Attribute::make(
+            set: fn ($value) => convertJsonDate($value),
+        );
+    }
+
 }
