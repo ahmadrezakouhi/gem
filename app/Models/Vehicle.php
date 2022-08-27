a8a6bc96c4bad2b4f614f98c26e0a250d6c58c3b
ahmad reza kouhi <ahmadrezakouhi@gmail.com>
2022-08-27 10:02:49 +0430

    convert all iso datetime in Vehicle  model

--- app/Models/Vehicle.php
+++ app/Models/Vehicle.php
@@ -4,8 +4,46 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+
 
 class Vehicle extends Model
 {
     use HasFactory;
+    protected $guarded = ['id'];
+
+
+    protected function smartNumberExpire(): Attribute
+    {
+        return Attribute::make(
+            set: fn ($value) => convertJsonDate($value),
+        );
+    }
+
+    protected function inspectionExpire(): Attribute
+    {
+        return Attribute::make(
+            set: fn ($value) => convertJsonDate($value),
+        );
+    }
+
+
+
+    protected function insuranceExpire(): Attribute
+    {
+        return Attribute::make(
+            set: fn ($value) => convertJsonDate($value),
+        );
+    }
+
+
+
+
+    protected function documentDate(): Attribute
+    {
+        return Attribute::make(
+            set: fn ($value) => convertJsonDate($value),
+        );
+    }
+
 }
