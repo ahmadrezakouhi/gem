56e5c73289b2b13bc9762fc2d65f15d367b91c84
ahmad reza kouhi <ahmadrezakouhi@gmail.com>
2022-08-27 09:58:42 +0430

    implement relationship between Company and Driver and Vehicle and SenderReciver all in Company model

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -9,4 +9,17 @@ class Company extends Model
 {
     use HasFactory;
     protected $guarded = ['id'];
+
+    public function drivers(){
+        return $this->hasMany(Driver::class,'panel_code','panel_code');
+    }
+
+    public function vehicles(){
+        return $this->hasMany(Vehicle::class,'panel_code','panel_code');
+    }
+
+    public function senders_receivers(){
+        return $this->hasMany(SenderReciver::class,'panel_code','panel_code');
+    }
+
 }
