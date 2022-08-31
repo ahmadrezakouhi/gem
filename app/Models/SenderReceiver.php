79bbc3f0f84c13875de9647de4a74e8552e819cd
ahmad reza kouhi <ahmadrezakouhi@gmail.com>
2022-08-31 15:19:04 +0430

    rename class name and other that use sender reciver to sender receiver  in SenderReceiver  model

--- app/Models/SenderReceiver.php
+++ app/Models/SenderReceiver.php
@@ -5,9 +5,13 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class SenderReciver extends Model
+class SenderReceiver extends Model
 {
     use HasFactory;
     protected $guarded = ['id'];
-    protected $table = 'senders_recivers';
+    protected $table = 'senders_receivers';
+
+    public function addresses(){
+        return $this->hasMany(Address::class,'sender_receiver_id','id');
+    }
 }
