3e21221525ee8adbcc656d8a696cae2663f3b051
ahmad reza kouhi <ahmadrezakouhi@gmail.com>
2022-09-01 13:05:58 +0430

    change reciver name to receiver and add some fields

--- database/factories/DraftFactory.php
+++ database/factories/DraftFactory.php
@@ -4,7 +4,7 @@ namespace Database\Factories;
 
 use App\Models\Company;
 use App\Models\Driver;
-use App\Models\SenderReciver;
+use App\Models\SenderReceiver;
 use App\Models\Vehicle;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Ybazli\Faker\Facades\Faker;
@@ -21,18 +21,22 @@ class DraftFactory extends Factory
      */
     public function definition()
     {
-        $sender =fake()->randomElement(SenderReciver::where('status','!=',1)->get());
-        $receiver =fake()->randomElement(SenderReciver::where('status','!=',0)->get());
+        $sender =fake()->randomElement(SenderReceiver::where('status','!=',1)->get());
+        $receiver =fake()->randomElement(SenderReceiver::where('status','!=',0)->get());
         $cargoes = ['اثایه منزل','سوسیس و کالباس','لبنیات','کاشی','شیرآلات'];
         $first_driver = fake()->randomElement(Driver::all());
         $second_driver = fake()->randomElement(Driver::all());
         $vehicle = fake()->randomElement(Vehicle::all());
         $status_code = fake()->numberBetween(0,1);
         $status_titles = ['رد شده','تایید شده'];
+        $warehouse_type_title = ['سرد خانه','انبار نفت','گمرک','سایر'];
         return [
             'panel_code'=>Company::find(rand(1,20))->panel_code,
             'draft_number'=>fake()->numerify('########'),
+            'draft_serial'=>fake()->numerify('########'),
+            'bill_of_lading_number'=>fake()->numerify('##########'),
             'cargo_declaration_number'=>fake()->numerify('########'),
+            'cargo_declaration_date'=>fake()->iso8601(),
             'licence_number'=>fake()->numerify('########'),
             'cargo_code'=>fake()->numerify('#########'),
             'cargo_title'=>fake()->randomElement($cargoes),
@@ -54,6 +58,18 @@ class DraftFactory extends Factory
             'status_code'=>$status_code,
             'status_title'=>$status_titles[$status_code],
             'is_local_cancel'=>fake()->numberBetween(0,1),
+            'start_validity'=>fake()->iso8601(),
+            'end_validity'=>fake()->iso8601(),
+            'order_number'=>fake()->numerify('#########'),
+            'is_logistics'=>fake()->numberBetween(0,1),
+            'receipt'=>fake()->numberBetween(0,1),
+            'warehouse_code'=>fake()->numerify('#########'),
+            'warehouse_type_code'=>fake()->numerify('###########'),
+            'warehouse_type_title'=>fake()->randomElement($warehouse_type_title),
+            'document_code'=>fake()->numerify('###########'),
+            'document_date'=>fake()->iso8601(),
+            'loading_number_plate'=>fake()->numerify('##ص###'),
+            'loading_number_plate_zone'=>fake()->numberBetween(11,99),
             'sender_national_code'=>$sender->national_code,
             'sender_postal_code'=>$sender->postal_code,
             'sender_economic_code'=>$sender->economic_code,
@@ -75,7 +91,8 @@ class DraftFactory extends Factory
             'first_driver_phones'=>$first_driver->phones,
             'second_driver_fullname'=>$second_driver->name.' '. $second_driver->last_name,
             'second_driver_national_code'=>$second_driver->national_code,
-            'second_driver_smart_number'=>$second_driver->smart_code,
+            'second_driver_smart_number'=>$second_driver->smart_number,
+            'second_driver_health_card_expire'=>$second_driver->health_card_expire,
             'second_driver_phones'=>$second_driver->phones,
             'vehicle_smart_number'=>$vehicle->smart_number,
             'vehicle_number_plate'=>$vehicle->number_plate,
