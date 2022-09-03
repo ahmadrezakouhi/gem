f43558c72f193cc1446dc843662e0697d86e8ad9
ahmad reza kouhi <ahmadrezakouhi@gmail.com>
2022-09-03 11:55:41 +0430

    create bill_of_ladings migration and add fields

--- database/migrations/2022_08_28_142219_create_bill_of_ladings_table.php
+++ database/migrations/2022_08_28_142219_create_bill_of_ladings_table.php
@@ -15,6 +15,80 @@ return new class extends Migration
     {
         Schema::create('bill_of_ladings', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('panel_code');
+            $table->foreign('panel_code')->references('panel_code')->on('companies');
+            $table->unsignedBigInteger('bill_of_lading_serial')->nullable();
+            $table->unsignedBigInteger('bill_of_lading_number')->nullable();
+            $table->unsignedBigInteger('draft_number')->nullable();
+            $table->unsignedBigInteger('draft_serial')->nullable();
+            $table->tinyInteger('overburden')->default(0);
+            $table->timestamp('estimate_receive_date')->nullable();
+            $table->unsignedBigInteger('warehouse_code')->nullable();
+            $table->unsignedBigInteger('warehouse_type_code')->nullable();
+            $table->string('warehouse_type_title',50)->nullable();
+            $table->unsignedBigInteger('warehouse_fee')->nullable();
+            $table->unsignedBigInteger('loading_code')->nullable();
+            $table->string('loading_title',50)->nullable();
+            $table->unsignedBigInteger('scale_fee')->nullable();
+            $table->unsignedBigInteger('sleep_fee')->nullable();
+            $table->unsignedBigInteger('insurance_tax')->nullable();
+            $table->unsignedBigInteger('insurance_fee')->nullable();
+            $table->unsignedBigInteger('overload_fee')->nullable();
+            $table->unsignedBigInteger('commission')->nullable();
+            $table->unsignedBigInteger('advance_freight')->nullable();
+            $table->unsignedBigInteger('add_valorem_tax')->nullable();
+            $table->unsignedBigInteger('unload_fee')->nullable();
+            $table->unsignedBigInteger('terminal_fee')->nullable();
+            $table->unsignedBigInteger('cost')->nullable();
+            $table->unsignedBigInteger('driver_insurance_fee')->nullable();
+            $table->unsignedBigInteger('freight_collect')->nullable();
+            $table->unsignedBigInteger('freight_insurance')->nullable();
+            $table->unsignedBigInteger('fixed_cost')->nullable();
+            $table->text('description')->nullable();
+            $table->string('sender_national_code',20)->nullable();
+            $table->string('sender_postal_code',20)->nullable();
+            $table->string('sender_economic_code',20)->nullable();
+            $table->string('sender_fullname',255)->nullable();
+            $table->string('sender_city',50)->nullable();
+            $table->text('sender_address')->nullable();
+            $table->string('sender_phones',255)->nullable();
+            $table->string('receiver_national_code',20)->nullable();
+            $table->string('receiver_postal_code',20)->nullable();
+            $table->string('receiver_economic_code',20)->nullable();
+            $table->string('receiver_fullname',255)->nullable();
+            $table->string('receiver_city',50)->nullable();
+            $table->text('receiver_address')->nullable();
+            $table->string('receiver_phones',255)->nullable();
+            $table->string('first_driver_fullname',255)->nullable();
+            $table->string('first_driver_national_code',20)->nullable();
+            $table->string('first_driver_smart_number',20)->nullable();
+            $table->timestamp('first_driver_health_card_expire')->nullable();
+            $table->string('first_driver_phones',255)->nullable();
+            $table->string('second_driver_fullname',255)->nullable();
+            $table->string('second_driver_national_code',20)->nullable();
+            $table->string('second_driver_smart_number',20)->nullable();
+            $table->timestamp('second_driver_health_card_expire')->nullable();
+            $table->string('second_driver_phones',255)->nullable();
+            $table->string('vehicle_smart_number',10)->nullable();
+            $table->string('vehicle_number_plate',10)->nullable();
+            $table->string('vehicle_number_plate_zone',2)->nullable();
+            $table->timestamp('vehicle_insurance_expire')->nullable();
+            $table->timestamp('vehicle_inspection_expire')->nullable();
+            $table->string('vehicle_brand_title',50)->nullable();
+            $table->string('vehicle_loading_type',50)->nullable();
+            $table->boolean('vehicle_status')->default(false);
+            $table->string('field_1',255)->nullable();
+            $table->string('field_2',255)->nullable();
+            $table->string('field_3',255)->nullable();
+            $table->string('field_4',255)->nullable();
+            $table->string('field_5',255)->nullable();
+            $table->string('field_6',255)->nullable();
+            $table->string('field_7',255)->nullable();
+            $table->string('field_8',255)->nullable();
+            $table->string('field_9',255)->nullable();
+            $table->string('field_10',255)->nullable();
+            $table->string('field_11',255)->nullable();
+            $table->string('field_12',255)->nullable();
             $table->timestamps();
         });
     }
@@ -26,6 +100,9 @@ return new class extends Migration
      */
     public function down()
     {
+
+
         Schema::dropIfExists('bill_of_ladings');
+
     }
 };
