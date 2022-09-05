951dac930a1336f7cc21953cf6e7d5bca4de1902
ahmad reza kouhi <ahmadrezakouhi@gmail.com>
2022-09-05 15:00:51 +0430

    change all iso date in Insurance model

--- app/Models/Insurance.php
+++ app/Models/Insurance.php
@@ -2,10 +2,40 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Insurance extends Model
 {
     use HasFactory;
+    protected $guarded = ['id'];
+
+    protected function startDate(): Attribute
+    {
+        return Attribute::make(
+            set: fn ($value) => convertIsoDate($value),
+        );
+    }
+
+    protected function endDate(): Attribute
+    {
+        return Attribute::make(
+            set: fn ($value) => convertIsoDate($value),
+        );
+    }
+
+    protected function queryDate(): Attribute
+    {
+        return Attribute::make(
+            set: fn ($value) => convertIsoDate($value),
+        );
+    }
+
+    protected function issueDate(): Attribute
+    {
+        return Attribute::make(
+            set: fn ($value) => convertIsoDate($value),
+        );
+    }
 }
