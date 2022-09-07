2a402e318f4063865bce8b8cd20e93a7f9b672ba
ahmad reza kouhi <ahmadrezakouhi@gmail.com>
2022-09-07 14:17:29 +0430

    add fields to drafts_bill_of_ladings migration

--- database/migrations/2022_09_07_140614_create_drafts_bill_of_ladings_table.php
+++ database/migrations/2022_09_07_140614_create_drafts_bill_of_ladings_table.php
@@ -15,6 +15,15 @@ return new class extends Migration
     {
         Schema::create('drafts_bill_of_ladings', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('panel_code');
+            $table->foreign('panel_code')->references('panel_code')->on('companies');
+            $table->timestamp('issue_date')->nullable();
+            $table->string('draft_bill_of_lading_number',20)->nullable();
+            $table->unsignedBigInteger('from_number')->nullable();
+            $table->unsignedBigInteger('to_number')->nullable();
+            $table->string('serial',10)->nullable();
+            $table->string('status_code',50)->nullable();
+            $table->text('description')->nullable();
             $table->timestamps();
         });
     }
