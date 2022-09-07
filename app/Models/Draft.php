ab41ee4a3ba1782bbc34c36da1c13e0f55994a37
ahmad reza kouhi <ahmadrezakouhi@gmail.com>
2022-09-07 11:17:52 +0430

    implement guarded and casts property Draft model

--- app/Models/Draft.php
+++ app/Models/Draft.php
@@ -9,37 +9,37 @@ class Draft extends Model
 {
     use HasFactory;
 
+    protected $guarded = ['id'];
+    protected $casts = [
+        'cargo_declaration_date'=>'datetime',
+        'traffic'=>'boolean',
+        'traffic_zone'=>'boolean',
+        'incendiary'=>'boolean',
+        'status_code'=>'boolean',
+        'is_local_cancel'=>'boolean',
+        'is_logistics'=>'boolean',
+        'receipt'=>'boolean',
+        'document_date'=>'datetime',
+        'first_driver_health_card_expire'=>'datetime',
+        'second_driver_health_card_expire'=>'datetime',
+        'vehicle_insurance_expire'=>'datetime',
+        'vehicle_inspection_expire'=>'datetime',
+        'start_validity'=>'datetime',
+        'end_validity'=>'datetime'
+
+    ];
+
     public function company(){
         return $this->belongsTo(Company::class,'panel_code','panel_code');
     }
 
-    protected function cargoDeclarationDate(): Attribute
-    {
-        return Attribute::make(
-            set: fn ($value) => convertIsoDate($value),
-        );
-    }
-
-    protected function startValidity(): Attribute
-    {
-        return Attribute::make(
-            set: fn ($value) => convertIsoDate($value),
-        );
-    }
-
-    protected function endValidity(): Attribute
-    {
-        return Attribute::make(
-            set: fn ($value) => convertIsoDate($value),
-        );
-    }
 
 
-    protected function documentDate(): Attribute
-    {
-        return Attribute::make(
-            set: fn ($value) => convertIsoDate($value),
-        );
-    }
+
+
+    
+
+
+
 
 }
