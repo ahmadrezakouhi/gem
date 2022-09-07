b6522e79d04ec127ec998e3c681caf26dec8b1b1
ahmad reza kouhi <ahmadrezakouhi@gmail.com>
2022-09-07 14:18:26 +0430

    implement table property to DraftBillOfLading model

---
+++ app/Models/DraftBillOfLading.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class DraftBillOfLading extends Model
+{
+    use HasFactory;
+    protected $table = 'drafts_bill_of_ladings';
+}
