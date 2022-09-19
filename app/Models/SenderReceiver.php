92215720bdf10d3e2e4697e5d77ec7b722879fa0
ahmad reza kouhi <ahmadrezakouhi@gmail.com>
2022-09-19 16:21:18 +0430

    implement casts property in SenderReceiver model

--- app/Models/SenderReceiver.php
+++ app/Models/SenderReceiver.php
@@ -4,14 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 
 class SenderReceiver extends Model
 {
     use HasFactory;
     protected $guarded = ['id'];
     protected $table = 'senders_receivers';
-
+    protected $casts = [
+        'is_receiver'=>'boolean',
+        'is_sender'=>'boolean'
+    ];
     public function addresses(){
         return $this->hasMany(Address::class,'sender_receiver_id','id');
     }
+
+
+    
 }
