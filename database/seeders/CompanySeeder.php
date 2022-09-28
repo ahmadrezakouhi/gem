961b65b4a37b7c8ec3e72ef1223cc1d0bb9c3272
ahmad reza kouhi <ahmadrezakouhi@gmail.com>
2022-09-28 14:46:20 +0330

    ceate CompanySeeder and implement run()

---
+++ database/seeders/CompanySeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Company;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Ybazli\Faker\Facades\Faker;
+
+class CompanySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Company::create([
+            'panel_code'=>fake()->numerify('123456789'),
+            'name'=>'پیش فرض',
+            'company_code'=>fake()->numerify('#######'),
+            'phone'=>fake()->phoneNumber(),
+            'fax'=>fake()->phoneNumber(),
+            'mobile'=>Faker::mobile(),
+            'manager_name'=>Faker::firstName(),
+            'manager_last_name'=>Faker::lastName(),
+            'presentor_name'=>Faker::firstName(),
+            'presentor_last_name'=>Faker::lastName()
+
+
+        ]);
+    }
+}
