9e9e0ede5c8a1d8829f83542450e74b4ca49c2a4
ahmad reza kouhi <ahmadrezakouhi@gmail.com>
2022-10-04 15:39:29 +0330

    implement guarded and cast property in Order model

---
+++ app/Models/Order.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    use HasFactory;
+
+    protected $guarded = ['id'];
+
+    protected $casts = [
+        'order_date'=>'datetime',
+        'transaction_date'=>'datetime',
+        'request_date'=>'datetime'
+    ];
+}
