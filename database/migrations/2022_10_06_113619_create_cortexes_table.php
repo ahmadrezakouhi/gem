2012642f5d7e08e5e89a12fe5a78fa32b4b88fa4
ahmad reza kouhi <ahmadrezakouhi@gmail.com>
2022-10-06 12:24:18 +0330

    add fields to cortex migration

--- database/migrations/2022_10_06_113619_create_cortexes_table.php
+++ database/migrations/2022_10_06_113619_create_cortexes_table.php
@@ -15,6 +15,41 @@ return new class extends Migration
     {
         Schema::create('cortexes', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('panel_code')->index();
+            $table->foreign('panel_code')->references('panel_code')->on('companies');
+            $table->unsignedBigInteger('cortex_number')->nullable();
+            $table->timestamp('cortex_date')->nullable();
+            $table->unsignedBigInteger('order_number')->nullable();
+            $table->unsignedBigInteger('order_item')->nullable();
+            $table->string('cargo_title')->nullable();
+            $table->text('cargo_description')->nullable();
+            $table->unsignedBigInteger('package_code')->nullable();
+            $table->string('package_title')->nullable();
+            $table->unsignedInteger('weight')->nullable();
+            $table->unsignedInteger('quantity')->nullable();
+            $table->unsignedBigInteger('value')->nullable();
+            $table->string('owner_name')->nullable();
+            $table->string('owner_last_name')->nullable();
+            $table->string('loading_location')->nullable();
+            $table->boolean('traffic')->default(false);
+            $table->boolean('incendiary')->default(false);
+            $table->boolean('traffic_zone')->default(false);
+            $table->unsignedInteger('ramainder_order')->nullable();
+            $table->unsignedBigInteger('sender_code')->nullable();
+            $table->string('sender_name')->nullable();
+            $table->string('sender_last_name')->nullable();
+            $table->string('sender_city')->nullable();
+            $table->string('sender_national_code',20)->nullable();
+            $table->unsignedBigInteger('sender_storage_code')->nullable();
+            $table->string('sender_postal_code',10)->nullable();
+            $table->unsignedBigInteger('receiver_code')->nullable();
+            $table->string('receiver_name')->nullable();
+            $table->string('receiver_last_name')->nullable();
+            $table->string('receiver_city')->nullable();
+            $table->string('receiver_national_code',20)->nullable();
+            $table->unsignedBigInteger('receiver_storage_code')->nullable();
+            $table->string('receiver_postal_code',10)->nullable();
+            $table->text('description')->nullable();
             $table->timestamps();
         });
     }
