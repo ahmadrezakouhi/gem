bec03f51c932de1df9939c8b7c674be7eb212a25
ahmad reza kouhi <ahmadrezakouhi@gmail.com>
2022-10-16 12:54:58 +0330

    create keys array for permission in getPermissionsAttribute User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -56,8 +56,24 @@ class User extends Authenticatable
 
         try {
             $decrypted = Crypt::decryptString($value);
-            return $decrypted;
+            $keys = [
+                'create_order', 'edit_order', 'delete_order', 'create_draft', 'edit_draft', 'delete_draft',
+                'create_bill_of_lading', 'edit_bill_of_lading', 'delete_bill_of_lading', 'cancle_bill_of_lading',
+                'cancle_local_bill_of_lading', 'delete_bill_of_lading', 'edit_bill_of_lading_form', 'print_bill_of_lading',
+                'access_report_form', 'create_sender_receiver', 'edit_sender_receiver', 'delete_sender_receiver',
+                'access_admin_panel', 'organization_report', 'user_management', 'user_permission', 'create_draft_bill_of_lading',
+                'edit_draft_bill_of_lading', 'delete_draft_bill_of_lading', 'create_owner', 'edit_owner', 'delete_owner',
+                'create_financialـtransaction', 'edit_financialـtransaction', 'delete_financialـtransaction', 'send_messsage',
+                'rent_management', 'insurance_management', 'financial_coefficients_management', 'receipt_management',
+                'default_bill_of_lading_management', 'print_form_management', 'company_management', 'company_management', 'design_reports',
+                'backup', 'recovery', 'create_bill_of_lading_file', 'upload_bill_of_lading_file', 'acountant_file', 'report', 'report_tax',
+                'chart', 'edit_owner_vehicle', 'create_report', 'edit_report', 'delete_report'
+
+            ];
+            return array_combine($keys, str_split($decrypted));
         } catch (DecryptException $e) {
+
+            
         }
     }
 
@@ -66,7 +82,4 @@ class User extends Authenticatable
     {
         $this->attributes['permissions'] = Crypt::encryptString($value);
     }
-
-
-
 }
