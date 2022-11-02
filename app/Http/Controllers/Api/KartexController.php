7ac5a032e683399d94e55fb988fb6cd1babff794
ahmad reza kouhi <ahmadrezakouhi@gmail.com>
2022-11-02 15:22:55 +0330

    add swagger document for KartexController

--- app/Http/Controllers/Api/KartexController.php
+++ app/Http/Controllers/Api/KartexController.php
@@ -11,45 +11,415 @@ use Symfony\Component\HttpFoundation\Response;
 class KartexController extends Controller
 {
     /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+     * @OA\Get(
+     *   path="/api/kartexes",
+     *   tags={"kartexes"},
+     *   summary="get all kartexes",
+     *       description="get all kartexes",
+     *   @OA\Response(
+     *      response=200,
+     *       description="Success",
+     *      @OA\MediaType(
+     *           mediaType="application/json",
+     *      )
+     *   ),
+     *   @OA\Response(
+     *      response=404,
+     *      description="not found"
+     *   ),
+     *)
+     **/
     public function index()
     {
         return KartexResource::collection(Kartex::paginate());
     }
 
-    /**
-     * Store a newly created resource in storage.
+      /**
+     * @OA\Post(
+     *   path="/api/kartexes",
+     *   tags={"kartexes"},
+     *   summary="create kartex",
+     *       description="create kartex",
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
+     *
+     *   @OA\RequestBody(
+     *     required=true,
+     *     @OA\MediaType(
+     *       mediaType="application/json",
+     *       @OA\Schema(
+     *          @OA\Property(
+     *           property="panel_code",
+     *           type="integer",
+     *         ),
+     *          @OA\Property(
+     *           property="kartex_number",
+     *           type="integer",
+     *         ),
+     *           @OA\Property(
+     *           property="kartex_date",
+     *           type="date",
+     *         ),
+     *          @OA\Property(
+     *           property="order_number",
+     *           type="integer",
+     *         ),
+     *           @OA\Property(
+     *           property="order_item",
+     *           type="integer",
+     *         ),
+     *
+     *           @OA\Property(
+     *           property="title",
+     *           type="string",
+     *         ),
+     *          @OA\Property(
+     *           property="explanation",
+     *           type="string",
+     *         ),
+     *           @OA\Property(
+     *           property="package_code",
+     *           type="integer",
+     *         ),
+     *          @OA\Property(
+     *           property="package_title",
+     *           type="string",
+     *         ),
+     *          @OA\Property(
+     *           property="weight",
+     *           type="integer",
+     *         ),
+     *          @OA\Property(
+     *           property="quantity",
+     *           type="integer",
+     *         ),
+     *          @OA\Property(
+     *           property="value",
+     *           type="integer",
+     *         ),
+     *          @OA\Property(
+     *           property="owner",
+     *           type="string",
+     *         ),
+     *          @OA\Property(
+     *           property="loading_location",
+     *           type="string",
+     *         ),
+     *          @OA\Property(
+     *           property="traffic_status",
+     *           type="boolean",
+     *         ),
+     *          @OA\Property(
+     *           property="fire_status",
+     *           type="boolean",
+     *         ),
+     *          @OA\Property(
+     *           property="plan_status",
+     *           type="boolean",
+     *         ),
+     *          @OA\Property(
+     *           property="remainder_order",
+     *           type="integer",
+     *         ),
+     *          @OA\Property(
+     *           property="sender_code",
+     *           type="integer",
+     *         ),
+     *          @OA\Property(
+     *           property="sender_name",
+     *           type="string",
+     *         ),
+     *          @OA\Property(
+     *           property="sender_last_name",
+     *           type="string",
+     *         ),
+     *          @OA\Property(
+     *           property="sender_city",
+     *           type="string",
+     *         ),
+     *          @OA\Property(
+     *           property="sender_national_code",
+     *           type="string",
+     *         ),
+     *          @OA\Property(
+     *           property="sender_storage_code",
+     *           type="integer",
+     *         ),
+     *          @OA\Property(
+     *           property="sender_postal_code",
+     *           type="string",
+     *         ),
+     *          @OA\Property(
+     *           property="receiver_code",
+     *           type="integer",
+     *         ),
+     *          @OA\Property(
+     *           property="receiver_name",
+     *           type="string",
+     *         ),
+     *          @OA\Property(
+     *           property="receiver_last_name",
+     *           type="string",
+     *         ),
+     *          @OA\Property(
+     *           property="receiver_city",
+     *           type="string",
+     *         ),
+     *          @OA\Property(
+     *           property="receiver_national_code",
+     *           type="string",
+     *         ),
+     *          @OA\Property(
+     *           property="receiver_storage_code",
+     *           type="integer",
+     *         ),
+     *          @OA\Property(
+     *           property="receiver_postal_code",
+     *           type="string",
+     *         ),
+     *         @OA\Property(
+     *           property="description",
+     *           type="string",
+     *         ),
+     *       ),
+     *     ),
+     *   ),
+     *
+     *
+     *
+     *
+     *
+     *   @OA\Response(
+     *      response=201,
+     *       description="Success",
+     *      @OA\MediaType(
+     *           mediaType="application/json",
+     *      )
+     *   ),
+     *   @OA\Response(
+     *      response=404,
+     *      description="not found"
+     *   ),
+     *)
+     **/
     public function store(Request $request)
     {
         $kartex = Kartex::create($request->all());
         return response()->json(new KartexResource($kartex),Response::HTTP_CREATED);
     }
 
-    /**
-     * Display the specified resource.
+     /**
+     * @OA\Get(
+     *   path="/api/kartexes/{kartex}",
+     *   tags={"kartexes"},
+     *   summary="get kartex by id",
+     *       description="get kartex by id",
+     *   @OA\Parameter(
+     *      name="kartex",
+     *      in="path",
+     *      required=true,
+     *      @OA\Schema(
      *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+     *          type="integer"
+     * )
+     *
+     *
+     * ),
+     *   @OA\Response(
+     *      response=202,
+     *       description="Success",
+     *      @OA\MediaType(
+     *           mediaType="application/json",
+     *      )
+     *   ),
+     *   @OA\Response(
+     *      response=404,
+     *      description="not found"
+     *   ),
+     *)
+     **/
     public function show(Kartex $kartex)
     {
         return response()->json(new KartexResource($kartex),Response::HTTP_ACCEPTED);
     }
 
     /**
-     * Update the specified resource in storage.
+     * @OA\Patch(
+     *   path="/api/kartexes/{kartex}",
+     *   tags={"kartexes"},
+     *   summary="update kartex",
+     *       description="update kartex",
+     *     @OA\Parameter(
+     *          name="kartex",
+     *          in="path",
+     *          required=true,
+     *          @OA\Schema(
+     *              type="integer"
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+     * )
+     *
+     * ),
+     *
+     *
+     *   @OA\RequestBody(
+     *     required=true,
+     *     @OA\MediaType(
+     *       mediaType="application/json",
+     *       @OA\Schema(
+     *          @OA\Property(
+     *           property="panel_code",
+     *           type="integer",
+     *         ),
+     *          @OA\Property(
+     *           property="kartex_number",
+     *           type="integer",
+     *         ),
+     *           @OA\Property(
+     *           property="kartex_date",
+     *           type="date",
+     *         ),
+     *          @OA\Property(
+     *           property="order_number",
+     *           type="integer",
+     *         ),
+     *           @OA\Property(
+     *           property="order_item",
+     *           type="integer",
+     *         ),
+     *
+     *           @OA\Property(
+     *           property="title",
+     *           type="string",
+     *         ),
+     *          @OA\Property(
+     *           property="explanation",
+     *           type="string",
+     *         ),
+     *           @OA\Property(
+     *           property="package_code",
+     *           type="integer",
+     *         ),
+     * *          @OA\Property(
+     *           property="package_title",
+     *           type="string",
+     *         ),
+     *          @OA\Property(
+     *           property="weight",
+     *           type="integer",
+     *         ),
+     *          @OA\Property(
+     *           property="quantity",
+     *           type="integer",
+     *         ),
+     *          @OA\Property(
+     *           property="value",
+     *           type="integer",
+     *         ),
+     *          @OA\Property(
+     *           property="owner",
+     *           type="string",
+     *         ),
+     *          @OA\Property(
+     *           property="loading_location",
+     *           type="string",
+     *         ),
+     *          @OA\Property(
+     *           property="traffic_status",
+     *           type="boolean",
+     *         ),
+     *          @OA\Property(
+     *           property="fire_status",
+     *           type="boolean",
+     *         ),
+     *          @OA\Property(
+     *           property="plan_status",
+     *           type="boolean",
+     *         ),
+     *          @OA\Property(
+     *           property="remainder_order",
+     *           type="integer",
+     *         ),
+     *          @OA\Property(
+     *           property="sender_code",
+     *           type="integer",
+     *         ),
+     *          @OA\Property(
+     *           property="sender_name",
+     *           type="string",
+     *         ),
+     *          @OA\Property(
+     *           property="sender_last_name",
+     *           type="string",
+     *         ),
+     *          @OA\Property(
+     *           property="sender_city",
+     *           type="string",
+     *         ),
+     *          @OA\Property(
+     *           property="sender_national_code",
+     *           type="string",
+     *         ),
+     *          @OA\Property(
+     *           property="sender_storage_code",
+     *           type="integer",
+     *         ),
+     *          @OA\Property(
+     *           property="sender_postal_code",
+     *           type="integer",
+     *         ),
+     *          @OA\Property(
+     *           property="receiver_code",
+     *           type="integer",
+     *         ),
+     *          @OA\Property(
+     *           property="receiver_name",
+     *           type="string",
+     *         ),
+     *          @OA\Property(
+     *           property="receiver_last_name",
+     *           type="string",
+     *         ),
+     *          @OA\Property(
+     *           property="receiver_city",
+     *           type="string",
+     *         ),
+     *          @OA\Property(
+     *           property="receiver_national_code",
+     *           type="string",
+     *         ),
+     *          @OA\Property(
+     *           property="receiver_storage_code",
+     *           type="integer",
+     *         ),
+     *          @OA\Property(
+     *           property="receiver_postal_code",
+     *           type="integer",
+     *         ),
+     *          @OA\Property(
+     *           property="description",
+     *           type="string",
+     *         ),
+     *
+     *      ),
+     *     ),
+     *   ),
+     *
+     *
+     *
+     *
+     *
+     *   @OA\Response(
+     *      response=201,
+     *       description="Success",
+     *      @OA\MediaType(
+     *           mediaType="application/json",
+     *      )
+     *   ),
+     *   @OA\Response(
+     *      response=404,
+     *      description="not found"
+     *   ),
+     *)
+     **/
     public function update(Request $request, Kartex $kartex)
     {
         $kartex->update($request->all());
@@ -57,11 +427,35 @@ class KartexController extends Controller
     }
 
     /**
-     * Remove the specified resource from storage.
+     * @OA\Delete(
+     *   path="/api/kartexes/{kartex}",
+     *   tags={"kartexes"},
+     *   summary="delete kartex by id",
+     *       description="delete kartex by id",
+     *   @OA\Parameter(
+     *      name="kartex",
+     *      in="path",
+     *      required=true,
+     *      @OA\Schema(
      *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+     *          type="integer"
+     * )
+     *
+     *
+     * ),
+     *   @OA\Response(
+     *      response=204,
+     *       description="Success (without content)",
+     *      @OA\MediaType(
+     *           mediaType="application/json",
+     *      )
+     *   ),
+     *   @OA\Response(
+     *      response=404,
+     *      description="not found"
+     *   ),
+     *)
+     **/
     public function destroy(Kartex $kartex)
     {
         $kartex->delete();
