b8176c4d627dfb00fe3f21e33266593b057cc130
ahmad reza kouhi <ahmadrezakouhi@gmail.com>
2022-11-23 16:04:09 +0330

    add relationship to Company in Driver model

--- app/Models/Driver.php
+++ app/Models/Driver.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Models;
+
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -10,22 +11,20 @@ class Driver extends Model
     use HasFactory;
     protected $guarded = ['id'];
     protected $casts = [
-        'driver_licence_expire'=>'datetime',
-        'health_card_expire'=>'datetime',
-        'smart_number_expire'=>'datetime',
-        'birth_date'=>'datetime',
-        'status'=>'boolean',
-        'active'=>'boolean',
-        'start_activity'=>'datetime',
-        'last_inquiry_date'=>'datetime',
+        'driver_licence_expire' => 'datetime',
+        'health_card_expire' => 'datetime',
+        'smart_number_expire' => 'datetime',
+        'birth_date' => 'datetime',
+        'status' => 'boolean',
+        'is_active' => 'boolean',
+        'start_activity' => 'datetime',
+        'last_inquiry_date' => 'datetime',
     ];
 
 
 
-
-
-
-
-
-
+    public function companies()
+    {
+        return $this->belongsToMany(Company::class, 'company_driver','driver_id','panel_code');
+    }
 }
