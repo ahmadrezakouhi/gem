f669eaf42ea9ac187f2e374da851d5b0aa27892f
ahmad reza kouhi <ahmadrezakouhi@gmail.com>
2022-11-28 15:47:05 +0330

    implement hidden , timpstamp attribute and add new element to cast in CompanyDriver

--- app/Models/CompanyDriver.php
+++ app/Models/CompanyDriver.php
@@ -9,9 +9,14 @@ class CompanyDriver extends Pivot
 {
     use HasFactory;
 
+    protected $hidden = ['id','panel_code','driver_id','created_at','updated_at'];
+
+    protected $timestamp = true;
+
     protected $casts = [
         'birth_date' => 'datetime',
         'health_card_expire'=>'datetime',
-        'start_activity'=>'datetime'
+        'start_activity'=>'datetime',
+        'status' => 'boolean'
     ];
 }
