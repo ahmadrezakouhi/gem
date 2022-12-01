a52d452934f2f4dcd6ec6e9c31c0f3afd53d6798
ahmad reza kouhi <ahmadrezakouhi@gmail.com>
2022-12-01 09:50:13 +0330

    change relationship Vehicle in Company model

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -4,11 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Schema;
 
 class Company extends Model
 {
     use HasFactory;
     protected $guarded = ['id'];
+
+    protected $primaryKey = 'panel_code';
 
     public function drivers()
     {
@@ -17,8 +20,10 @@ class Company extends Model
 
     public function vehicles()
     {
-        return $this->hasMany(Vehicle::class, 'company_vehicle', 'panel_code', 'vehicle_id')
-            ->using(CompanyVehicle::class)->as('company_vehicle');
+        $columns = Schema::getColumnListing('company_vehicle');
+        
+        return $this->belongsToMany(Vehicle::class, 'company_vehicle', 'panel_code', 'vehicle_id')
+            ->using(CompanyVehicle::class)->withPivot($columns)->as('company_vehicle_info');
     }
 
     public function senders_receivers()
